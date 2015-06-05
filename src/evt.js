cec7c4f9a82ce1f8aef5ea72a96ab075a5ea8352
Marc Costello <cossie7722@gmail.com>
2015-06-05 13:26:03 +0100

    Adding some structure, and a few more requirements

--- src/evt.js
+++ src/evt.js
@@ -10,9 +10,41 @@
       .off();
       .raise();
 
-      namespacing, e.g: evt(element).on("click.mynamespace", handler);
-      filtering, e.g: evt(document).on("click", "#my-element", handler);
-      raise/trigger, e.g: evt(document).raise("my-event", payload);
+      - namespacing, e.g: evt(element).on("click.mynamespace", handler);
+      - filtering, e.g: evt(document).on("click", "#my-element", handler);
+      - raise/trigger, e.g: evt(document).raise("my-event", payload);
+
+      - should we cache dom selectors? we should make sure the gc can still clean up.
    */
+
+   function ext() {
+
+   }
+
+   ext.prototype.on = function() {
+      // todo
+   };
+
+   ext.prototype.one = function() {
+      // todo
+   };
+
+   ext.prototype.off = function() {
+      // todo
+   };
+
+   ext.prototype.raise = function() {
+      // todo
+   };
+
+   function extInit(element) {
+
+      // handle the element/selector
+
+      return new ext();
+   }
+
+   // or however we decide to export it.
+   module.exports = extInit;
 
 })();
