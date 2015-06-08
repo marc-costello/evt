678e9405059a5ac1a711523577bbee1f7efde275
Marc Costello <cossie7722@gmail.com>
2015-06-08 09:12:24 +0100

    Added check for string. Removed some comments too.

--- src/evt.js
+++ src/evt.js
@@ -28,11 +28,10 @@ evt.prototype.raise = function() {
 };
 
 function evtInit(element) {
-  if (!element || (element instanceof Element) === false) {
+  if (!element || (element instanceof Element) === false || typeof element !== 'string') {
     throw new Error('You must supply a DOM element or selector to evt');
   }
 
-  // first lets get on the same page. If it's not an Element already. querySelectorAll for it.
   var resolvedElements;
   if (!(element instanceof Element)) {
     resolvedElements = document.querySelectorAll(element);
@@ -41,10 +40,8 @@ function evtInit(element) {
   }
 
   for (var i=0; i < resolvedElements.length; i++) {
-    // now we need to check this element for an existing evt attribute
     var elementToken = resolvedElements[i].getAttribute(evtAttributeName);
     if (!elementToken) {
-      // generate one and attach it to the element.
       elementToken = generateToken();
       resolvedElements[i].setAttribute(evtAttributeName, elementToken);
     }
